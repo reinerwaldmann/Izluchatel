2f2eaec1d0e9de965860549a37c3a546e47c648e
reinerwaldmann <mksd@list.ru>
2013-10-01 12:11:53 +0400

    product shell update, minor code

--- Izluchatel/productshell.cpp
+++ Izluchatel/productshell.cpp
@@ -1,14 +1,58 @@
 #include "productshell.h"
 #include "ui_productshell.h"
 
-ProductShell::ProductShell(QWidget *parent) :
+ProductShell::ProductShell(char iDebugLevel, QWidget *parent):
     QWidget(parent),
     ui(new Ui::ProductShell)
 {
     ui->setupUi(this);
+    debugLevel = iDebugLevel;
+
+
 }
 
 ProductShell::~ProductShell()
 {
     delete ui;
 }
+
+
+void ProductShell::test()
+{}
+
+void ProductShell::pause()
+{}
+
+void ProductShell::writeConsole(QString msg, char type)
+{
+
+    switch (type)
+    {
+
+    case MSG_ERROR:
+        ui->textEditShell->append(tr( "<font color-red> %1 </font> <br>" ).arg(msg)  );
+        break;
+    case MSG_GOOD:
+        ui->textEditShell->append(tr( "<font color-green> %1 </font> <br>" ).arg(msg)  );
+        break;
+    case MSG_NEUTRAL:
+        ui->textEditShell->append(tr( " %1 <br> " ).arg(msg)  );
+        break;
+
+
+
+    }
+
+
+}
+
+
+
+
+
+void ProductShell::timeoutDeviceUnreachable()
+{}
+
+
+void ProductShell::acceptResultSlot(QString str)
+{}
--- Izluchatel/productshell.h
+++ Izluchatel/productshell.h
@@ -2,21 +2,55 @@
 #define PRODUCTSHELL_H
 
 #include <QWidget>
+#include "principalwindow.h"
+
+#define MSG_ERROR=1
+#define MSG_NEUTRAL=0;
+#define MSG_GOOD=2;
+
+#define DEBUG_N=0;
+#define DEBUG_V=1;
+
+
 
 namespace Ui {
 class ProductShell;
 }
+
+class PrincipalWindow;
 
 class ProductShell : public QWidget
 {
     Q_OBJECT
     
 public:
-    explicit ProductShell(QWidget *parent = 0);
+    QTimer timer;
+    char debugLevel;
+    bool flagWaitingAnswer;  //if we sent smf to the port, and waiting for answer,
+    //for result, for example
+    PrincipalWindow * principal;
+
+
+
+    explicit ProductShell(char iDebugLevel=0, QWidget *parent = 0);
     ~ProductShell();
+    void writeConsole (QString msg, char type=0);
+    void test (); //begin testing
+    void pause(); //pause testing
+
+
     
 private:
     Ui::ProductShell *ui;
+
+
+private slots:
+    void acceptResultSlot (QString str);
+    void timeoutDeviceUnreachable ();
+
+
+
+
 };
 
 #endif // PRODUCTSHELL_H
